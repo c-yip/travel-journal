8d6816255785be1dd645671a3c0b1d7b375d8c16
Christian <christianyip92@gmail.com>
2022-09-26 12:28:05 -0700

    Add header and App components.

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <img src={require('../globe.png')} alt="globe"></img>
+      <h1>c-yip's travel journal.</h1>
+    </nav>
+  )
+}

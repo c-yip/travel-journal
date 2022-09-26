70d006e001311e14d79b1b5d96d0f4592777599f
Christian <christianyip92@gmail.com>
2022-09-26 13:44:30 -0700

    Add props to cards component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Nav from './components/Nav';
 import Cards from './components/Cards';
+import data from './data'
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Cards 
+        key={item.id}
+        {...item}
+      />
+    )
+  })
+  
   return (
     <div>
       <Nav />
-      <Cards />
+      <section className='cards-section'>
+        {cards}
+      </section>
+      
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,20 @@
-export default function Cards() {
+export default function Cards(props) {
+  const location = props.location.toUpperCase();
+  
   return (
-    <p>cards</p>
+    <div className="card">
+      <img src={require(`../${props.imageUrl}`)} alt={'props.title'}/>
+      
+      <div className="card-content">
+        <div className="location-container">
+          <img src={require('../pin.jpg')} alt="pin"></img>
+          <p>{location}</p>
+          <a href={props.googleMapsUrl}>View on Google Maps</a>
+        </div>
+        <h1 className="card-title">{props.title}</h1>
+        <p className="dates">{`${props.startDate} - ${props.endDate}`}</p>
+        <p className="description">{props.description}</p>
+      </div>
+    </div>
   )
 }
